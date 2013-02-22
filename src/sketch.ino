7c7a2c332c91afdf2728aeba343666013d5bac7f
Max Winde <max.winde@gmail.com>
2013-02-22 15:55:04 +0100

    it's fading lovely

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,36 +1,53 @@
 const int LED = 13;
 
-const int LED1red = 2;
-const int LED1blue = 3;
-const int LED1green = 4;
+const int LED1red = 3;
+const int LED1blue = 5;
+const int LED1green = 6;
 
-const int LED2red = 5;
-const int LED2blue = 6;
-const int LED2green = 7;
+const int LED2red = 9;
+const int LED2blue = 10;
+const int LED2green = 11;
 
 const int pins[7] = {LED, LED2red, LED1red, LED2green, LED1green, LED2blue, LED1blue};
 
-int value = 0;
+float value = 0;
+
+int red = 0;
+int blue = 0;
+int green = 0;
 
 void setup() {
+    Serial.begin(9600);
 	for (int i = 0; i < 7; ++i)
 	{
 		pinMode(pins[i], OUTPUT);
 	}
 }
 
+int sinToPin(float value) {
+	return sin(value) * 128 + 127; 
+}
+
 void loop() {
-	value += 2;
+	value += 0.04;
+	red = sinToPin(value);
+	green = sinToPin(value + 1.5);
+	blue = sinToPin(value + 3.0);
 
-	if (value >= 255)
-	{
-		value = 0;
-	}
 
-	for (int i = 0; i < 7; ++i)
-	{
-		int pin = pins[i];
-		analogWrite(pin, value);
-	}
-	delay(10);
+	Serial.println(red);
+
+	analogWrite(LED, red);
+	analogWrite(LED1red, red);
+	analogWrite(LED2red, green);
+	analogWrite(LED1green, green);
+	analogWrite(LED2green, blue);
+	analogWrite(LED1blue, blue);
+	analogWrite(LED2blue, red);
+	// for (int i = 0; i < 7; ++i)
+	// {
+	// 	int pin = pins[i];
+	// 	analogWrite(pin, value);
+	// }
+	delay(20);
 }
